af2722d6b30c350f4444299f3c958b96b2260e1a
Felipe Moraes <elpmoraes@gmail.com>
2021-02-17 14:01:37 -0300

    Create seculoAno.php

---
+++ seculoAno.php
@@ -0,0 +1,36 @@
+<?php
+<!-- Desenvolva uma função que receba como parametro o ano e retorne o século ao qual este ano faz parte. 
+O primeiro século começa no ano 1 e termina no ano 100, o segundo século começa no ano 101 e termina no 200.
+
+Exemplos para teste:
+
+Ano 1905 = século 20
+Ano 1700 = século 17 -->
+
+
+function seculoAno($ano){
+
+            if($ano > 0 && $ano < 9999){
+
+        if($ano % 10 >= 1 && $ano % 10 < 9){
+             $seculo = $ano / 100 + 1;
+       
+        }else{
+                    echo "Termina em 0";
+              $seculo = $ano / 100;
+        }
+        $seculo = number_format($seculo, 0, '.', '');
+        echo "Ano ".$ano." = século ".$seculo;
+     
+     
+
+
+
+    }else{
+        echo "Ano deve estar entre 1 e 9999";
+    }
+
+
+}
+
+?>
